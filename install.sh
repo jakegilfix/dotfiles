6885a878eb2d7616d1e93f45e73a4e40e7593933
Jake Gilfix <jake.gilfix@affirm.com>
2025-06-21 01:50:30 -0700

    feat: sets default shell to fish and installs utils

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Get more recent version of `fish`
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+sudo apt update
+
+# Install stuff I want
 sudo apt install -y zsh bat fzf fish zoxide
 
-sudo chsh -s $(which zsh) affirm
-sudo chsh -s $(which zsh) jakegilfix
+# Alias bat (wth is batcat?)
+sudo ln -s /usr/bin/batcat /usr/bin/bat
 
+# Set default shell to fish
+which fish && sudo chsh -s $(which fish) affirm
+which fish && sudo usermod -s $(which fish) jakegilfix
+
+# Create symlinks to dotfiles
 ln -s "$HOME/.dotfiles/.config" "$HOME/.config"
 ln -s "$HOME/.dotfiles/.zprofile" "$HOME/.zprofile"
 
+# Install starship (prompt)
+curl -sS https://starship.rs/install.sh | sudo sh
+
+# Install `eza`
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
