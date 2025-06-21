e538073053d734b266323ef171d86e6aefbeaa92
Jake Gilfix <jake.gilfix@affirm.com>
2025-06-20 23:46:48 -0700

    feat: adds fish to install script

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo apt install -y zsh bat fzf
+sudo apt install -y zsh bat fzf fish
 
 sudo chsh -s $(which zsh) affirm
 sudo chsh -s $(which zsh) jakegilfix
 
 ln -s "$HOME/.dotfiles/.config" "$HOME/.config"
 ln -s "$HOME/.dotfiles/.zprofile" "$HOME/.zprofile"
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
